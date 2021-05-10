ed6c49877188433069a825e6abb80db72cf01d95
maisaa <maisaa.alkhedr@gmail.com>
2021-05-10 12:14:52 +0200

    add post user API

--- index.js
+++ index.js
@@ -3,6 +3,51 @@ const app = express();
 
 const port = 3000;
 
-app.listen(port, ()=>{
+app.use(express.json());
+
+const users = [{ name: "sarah", age: 30 },
+{ name: "John", age: 25 },
+{ name: "Jane", age: 20 },
+{ name: "Mark", age: 19 }];
+
+
+//get all
+app.get("/users", (req,res)=>{
+    res.status(200);
+    res.json(users);
+});
+
+
+//get one user
+app.get("/user/:name", (req,res)=>{
+
+    const userName = req.params.name
+    const found = users.find((ele)=>{
+        return ele.name === userName;
+    });
+    if(found){
+        res.status(200);
+        res.json(found.name);
+    } else {
+        res.status(404);
+        res.json("user is not found");
+    }
+
+});
+
+//post user
+app.post('/create/user',(req,res)=>{
+    const newUser = { name:req.body.name , age: req.body.age };
+
+    users.push(newUser);
+    res.status(200);
+    res.json(newUser);
+});
+
+//get first-user
+
+
+
+app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
