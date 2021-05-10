f799b27b2978f0f515f400352883fff034ce794e
maisaa <maisaa.alkhedr@gmail.com>
2021-05-10 13:54:08 +0200

    add get post put delet

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const { json } = require("express");
 const express = require("express");
 const app = express();
 const port = 4000;
 
 
-const todos = [{ todo: " wake up", isCompleted: false }, { todo: "Eat Breakfast", isCompleted: false }];
+app.use(express.json());
+
+const todos = [{ todo: "wake up", isCompleted: false }, { todo: "Eat Breakfast", isCompleted: false }];
 
 // get all todos list
 app.get("/todos", (req,res)=>{
@@ -12,9 +15,50 @@ app.get("/todos", (req,res)=>{
 })
 
 //post new todo
-app.post("/create/todo",(req,res)=>{
-    
-})
+app.post("/create/todo", (req,res) =>{
+    console.log("req.body.todo",req.body.todo);
+    const newTodo = {todo: req.body.todo , isCompleted: req.body.isCompleted};
+    todos.push(newTodo)
+    res.status(201);
+    res.json(newTodo);
+});
+
+//put todo
+app.put('/update/todo/:name', (req,res)=>{
+    let index;
+    const todo = req.params.name;
+    const found = todos.find((ele,i)=>{
+        index = i;
+        return ele.todo === todo; 
+    })
+    if(found){
+        // todos[index] = {todo : req.body.todo, isCompleted: req.body.isCompleted };
+        todos[index].isCompleted =  req.body.isCompleted ;
+        res.status(200);
+        res.json(todos[index]);
+    } else {
+        res.status(404);
+        res.json("todo not found");
+    }
+});
+
+//delete todo
+app.delete('/delete/todo/:name', (req,res)=>{
+    let index;
+    const todo = req.params.name;
+    const found = todos.find((ele,i)=>{
+        index = i;
+        return ele.todo === todo;
+    });
+    if(found){
+        todos.splice(index,1);
+        res.status(200);
+        res.json(todos);
+    } else {
+        res.status(404);
+        res.json("todo not exist");
+    }
+} )
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
